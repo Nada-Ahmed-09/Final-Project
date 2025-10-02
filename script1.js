f08fdf8d20df1afefb365a29bf1dcd3da291ed18
Nada-Ahmed-09 <nadablink1922@gmail.com>
2025-10-02 04:06:20 +0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,5 @@
+document.getElementById("LoginForm").addEventListener("submit", function(event) {
+      event.preventDefault(); 
+      
+      window.location.href = "main-page2.html";  
+    });
---
+++ script2.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("submit-btn");
+
+button.addEventListener("click" , doSomething);
+
+function doSomething(){
+    alert("Thanks For Sharing Your Favourite Animal!");
+}
